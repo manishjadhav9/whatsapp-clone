58126f702ad9a63e1bc44e8493874c843b89e0de
Manish Jadhav <manishsj289@gmail.com>
2024-04-20 17:10:58 +0530

    modified page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,5 @@
 import LeftPanel from "@/components/home/left-panel";
 import RightPanel from "@/components/home/right-panel";
-import { useTheme } from "next-themes";
 
 export default function Home() {
 	return (
